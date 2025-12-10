8e3308b3031938988ebe975e3666497c0e6249e4
nikitanimore992 <992nikitanimore@gmail.com>
2025-12-10 23:06:20 +0530

    add project

---
+++ src/main/java/com/nikku/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nikku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/","/home"})
+    public String showHomePage(){
+        return "home";
+    }
+}
